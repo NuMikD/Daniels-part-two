8ad1acd7ded230cd419a6ffb38452a35ad823c31
Michael <mdaniels6532@gmail.com>
2018-11-26 04:40:09 -0500

    display recommendations

---
+++ script.js
@@ -0,0 +1,33 @@
+var eventType= prompt('What type of event are you attending?');
+var tempFahr= prompt('What will the temperature be?');
+var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear ");
+
+//alert(result);
+// select.addEventListener('change', result);
+
+// function result() {
+ 
+
+  
+    if(eventType === 'casual' && tempFahr < 54) {
+         alert(result);
+      }
+//  else if (eventType = 'semi-formal' && tempFahr < 54) {
+//         document.write(result);
+//               } else if (eventType = 'formal' && tempFahr < 54) {
+//                 document.write(result);
+//                       } if(eventType === 'casual' && tempFahr < ) {
+//                         document.write(result);
+//                      } else if (eventType = 'semi-formal' && tempFahr < ) {
+//                        document.write(result);
+//                              } else if (eventType = 'formal' && tempFahr < ) {
+//                                document.write(result);
+//                                      } if(eventType === 'casual' && tempFahr < ) {
+//                                       document.write(result);
+//                                    } else if (eventType = 'semi-formal' && tempFahr < ) {
+//                                      document.write(result);
+//                                            } else if (eventType = 'formal' && tempFahr < ) {
+//                                              document.write(result);
+//                                                    } 
+
+// }
