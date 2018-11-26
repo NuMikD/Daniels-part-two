fb8617156c4badda6e5b287d6ba17fa4d49a85aa
Michael <mdaniels6532@gmail.com>
2018-11-26 06:43:34 -0500

    final js draft

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 var eventType= prompt('What type of event are you attending?');
 var tempFahr= prompt('What will the temperature be?');
-var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear ");
-
-//alert(result);
-// select.addEventListener('change', result);
-
-// function result() {
- 
-
+var clothes= ["something comfy", "a polo", "a suit", "a coat", "a jacket", "no jacket" ]
+var result;
   
-    if(eventType === 'casual' && tempFahr < 54) {
-         alert(result);
-      }
-//  else if (eventType = 'semi-formal' && tempFahr < 54) {
-//         document.write(result);
-//               } else if (eventType = 'formal' && tempFahr < 54) {
-//                 document.write(result);
-//                       } if(eventType === 'casual' && tempFahr < ) {
-//                         document.write(result);
-//                      } else if (eventType = 'semi-formal' && tempFahr < ) {
-//                        document.write(result);
-//                              } else if (eventType = 'formal' && tempFahr < ) {
-//                                document.write(result);
-//                                      } if(eventType === 'casual' && tempFahr < ) {
-//                                       document.write(result);
-//                                    } else if (eventType = 'semi-formal' && tempFahr < ) {
-//                                      document.write(result);
-//                                            } else if (eventType = 'formal' && tempFahr < ) {
-//                                              document.write(result);
-//                                                    } 
-
-// }
+if(eventType === 'casual' && tempFahr < 54) {
+  var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[0]+" and "+clothes[3]);
+  alert(result);
+  console.log(result);
+  } else if (eventType === 'semi-formal' && tempFahr < 54) {
+    var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[1]+" and "+clothes[3]);
+    alert(result);    
+    console.log(result);
+      } else if (eventType === 'formal' && tempFahr < 54) {
+        var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[2]+" and "+clothes[3]);
+        alert(result); 
+        console.log(result);
+          } else if(eventType === 'casual' && tempFahr >= 54 && tempFahr <= 70 ) {
+            var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[0]+" and "+clothes[4]+".");
+            alert(result);
+            console.log(result);
+              } else if (eventType === 'semi-formal' && tempFahr >= 54 && tempFahr <= 70 ) {
+                var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[1]+" and "+clothes[4]+".");
+                alert(result);
+                console.log(result);
+                  } else if (eventType === 'formal' && tempFahr >= 54 && tempFahr <= 70 ) {
+                    var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[2]+" and "+clothes[4]+".");
+                    alert(result);
+                    console.log(result);
+                      } else if(eventType === 'casual' && tempFahr > 70 ) {
+                        var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[0]+" and "+clothes[5]+".");
+                        alert(result); 
+                        console.log(result);
+                        } else if (eventType === 'semi-formal' && tempFahr > 70 ) {
+                          var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[1]+" and "+clothes[5]+".");
+                          alert(result); 
+                          console.log(result);
+                            } else if (eventType === 'formal' && tempFahr > 70 ) {
+                              var result = "".concat("Since it is "+tempFahr+" degrees and you are going to a "+eventType+" event, you should wear "+clothes[2]+" and "+clothes[5]+".");
+                              alert(result); 
+                              console.log(result);
+                              } 
